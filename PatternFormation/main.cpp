d1dedefe062570ce4c672e02c3b62af58efa40e6
Knishk Arora <knishkarora.dev@gmail.com>
2026-08-21 18:53:56 +0530

    TOPIC: covered some pattern till increasing then decreasing pattern.

--- PatternFormation/main.cpp
+++ PatternFormation/main.cpp
@@ -79,11 +79,68 @@ void print8 (int n){
     }
 }
 
+void print9 (int n){
+    for(int i=n;i>=1;i--){
+    	for(int a=1; a<n-i+1; a++){
+    		cout<<" ";
+		}
+		for(int j=1;j<=(2*i)-1;j++){
+		    cout<<"*";
+		}
+ 	    for(int b=1; b<n-i+1; b++){
+    		cout<<" ";
+		}
+        cout<<endl;
+    }
+}
+
+void print10 (int n){
+    for(int i=1;i<=n;i++){
+    	for(int a=n-i; a>0; a--){
+    		cout<<" ";
+		}
+		for(int j=1;j<=(2*i)-1;j++){
+		    cout<<"*";
+		}
+ 	    for(int b=n-i; b>0; b--){
+    		cout<<" ";
+		}
+        cout<<endl;
+    }
+        for(int i=n-1;i>=1;i--){
+    	for(int a=1; a<n-i+1; a++){
+    		cout<<" ";
+		}
+		for(int j=1;j<=(2*i)-1;j++){
+		    cout<<"*";
+		}
+ 	    for(int b=1; b<n-i+1; b++){
+    		cout<<" ";
+		}
+        cout<<endl;
+    }
+}
+
+void print11 (int n){
+    for(int i=n-1;i>=0;i--){
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+        for(int i=1;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
 
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int n;
     cin>>n;
-    print8(n);
+    print11(n);
 }
